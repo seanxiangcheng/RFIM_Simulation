10945e4bac8de2630fa0f3ce691568d606ee8391
XiangCheng <chengxiang.cn@gmail.com>
2015-09-22 11:06:46 -0400

    fig updates

--- figs_code/med_plots.py
+++ figs_code/med_plots.py
@@ -1,4 +1,16 @@
 import numpy as np
 import pandas as pd
-import 
+import matplotlib.pyplot as plt
+
+def get_med_fn(L, T, repeat, shi=1.0, nm='sqT'):
+    fname = "MED"
+    fname += "_L" + str(L) 
+    fname += "_T%.1f" % T
+    fname += "_shi%.1f" % shi
+    fname += "_sHi%.2f" % np.sqrt(T)
+    fname += "_r%d" % repeat
+    fname += "_" + nm + ".csv"
+    return(fname)
+    
+
 
